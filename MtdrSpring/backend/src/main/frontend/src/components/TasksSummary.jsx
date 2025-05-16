8aee3bb50e169a3fc68d1b26ade99678bd98ab55
LuzPaty <luzpatyhr@gmail.com>
2025-05-15 20:21:23 -0600

    pie chart task summary

--- MtdrSpring/backend/src/main/frontend/src/components/TasksSummary.jsx
+++ MtdrSpring/backend/src/main/frontend/src/components/TasksSummary.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import TaskPieChart from './charts/TaskPieChart';
 import API from '../API';
 
 export default function TasksSummary({ userId, userName }) {
@@ -47,6 +48,14 @@ export default function TasksSummary({ userId, userName }) {
         return () => clearInterval(intervalId);
     }, [userId]);
 
+    const completed = tasks.filter(task => task.done).length;
+    const overdue = tasks.filter(task => new Date(task.deadline) < new Date() && !task.done).length;
+    const pending = tasks.filter(task => !task.done && new Date(task.deadline) >= new Date()).length;
+    console.log('completed:', completed);
+    console.log('overdue:', overdue);
+    console.log('pending:', pending);
+
+
     return (
         <div >
             {loading ? (
@@ -56,13 +65,20 @@ export default function TasksSummary({ userId, userName }) {
                     {!userId && <h2>Tasks Summary</h2>}
                     {userId && <h2>Tasks Summary for {userName} (<span>{userId}</span>)</h2>}
                     <p>Total Tasks: {tasks.length}</p>
-                    <p>Completed Tasks: {tasks.filter(task => task.done).length}</p>
-                    <p>Pending Tasks: {tasks.filter(task => !task.done).length}</p>
-                    <p>
-                        Overdue Tasks: {tasks.filter(task =>
-                            new Date(task.dueDate) < new Date() && !task.done
-                        ).length}
-                    </p>
+                    <ul>
+                        <li>Completed Tasks: {tasks.filter(task => task.done).length}</li>
+                        <li>Pending Tasks: {tasks.filter(task => !task.done && new Date(task.deadline) >= new Date()).length}</li>
+                        <li>
+                            Overdue Tasks: {tasks.filter(task =>
+                                new Date(task.deadline) < new Date() && !task.done
+                            ).length}
+                        </li>
+                    </ul>
+                    <TaskPieChart
+                        completed={completed}
+                        pending={pending}
+                        overdue={overdue}
+                    />
                 </div>
             )}
         </div>
