6a80e66f42a50a4aa172523424c8fbcd74451204
LuzPaty <luzpatyhr@gmail.com>
2025-05-28 16:26:21 -0600

    cositas de imports uwu

--- MtdrSpring/backend/src/main/frontend/src/routes.jsx
+++ MtdrSpring/backend/src/main/frontend/src/routes.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ToDoPage from './pages/ToDoPage';
 import KpiDashboard from './pages/KpiDashboard';
 import ToDoList from './pages/ToDoList';
 
