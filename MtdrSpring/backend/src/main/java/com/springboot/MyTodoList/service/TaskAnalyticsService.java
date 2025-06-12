472f2c1db86c25aac6159f57a90476869ce4a63b
Jorge Arizpe <A01637441@tec.mx>
2025-06-12 01:43:36 -0600

    ya no se

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TaskAnalyticsService.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/service/TaskAnalyticsService.java
@@ -35,7 +35,7 @@ public class TaskAnalyticsService {
     @Autowired
     private SprintsRepository sprintsRepository;
     
-    @Value("${openai_api_key:}")
+    @Value("${openai_api_key}")
     private String openAiApiKey;
     
     private OpenAiService openAiService;
